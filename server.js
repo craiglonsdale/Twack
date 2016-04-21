47a2a255619a155dee633b957dee82dbe4ec8384
Craig Lonsdale <craig@meerkats.com.au>
2016-04-21 15:07:06 +0800

    Grabs a twitch username and updates chat room topic if a stream is available

--- server.js
+++ server.js
@@ -3,7 +3,5 @@ require("babel-register");
 const path = require('path');
 const argv = require('minimist')(process.argv.slice(2));
 const config = require(path.join(__dirname, argv.config));
-// other babel configuration, if necessary
 
-// load your app
 var app = require("./app")(config);
