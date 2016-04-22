7789ca704f6cbba2f0e277ed956fc43a6f419c82
Craig Lonsdale <craig@meerkats.com.au>
2016-04-22 15:38:09 +0800

    Added some basic functionality to 'leave' a stream

--- app/lib/twitch.js
+++ app/lib/twitch.js
@@ -1,10 +1,10 @@
-export function joinStream(twitchy, slack, slackToken, userName, channel) {
+export function joinStream(twitchy, slack, token, userName, channel) {
   return new Promise(function(resolve, reject) {
     twitchy._get(`streams/${userName}`, function(err, res) {
       const streamInfo = res.body;
       if (streamInfo.stream) {
         slack.channels.setTopic({
-          token: slackToken,
+          token: token,
           channel,
           topic: `${streamInfo.stream.channel.display_name} - ${streamInfo.stream.game}`
         }, () => {});
@@ -15,4 +15,12 @@ export function joinStream(twitchy, slack, slackToken, userName, channel) {
       }
     });
   })
+}
+
+export function leaveStream(slack, token, channel) {
+  return slack.channels.setTopic({
+    token: token,
+    channel,
+    topic: 'No current stream'
+  }, () => {});
 }
